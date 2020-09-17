4792d5ed5fe5d8221e8221994da9bc7ef3fff51e
Cristian Nasirabadi <cristian.tirdad@gmail.com>
2020-09-17 20:54:21 +0200

    transform translate and scale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
-
+import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlited', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal <=> highlited', animate(200))
+      // transition('highlited => normal', animate(200))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlited', style({
+        'background-color': 'green',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'blue',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('shrunken <=> *', animate(200))
+    ])
+
+  ]
 })
 export class AppComponent {
+  state = 'normal';
+  wildState = 'normal';
   title = 'angular-animation';
+
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlited' : this.state = 'normal';
+  }
+
+
+  onShrink() {
+    this.wildState === 'normal' ? this.wildState = 'shrunken' : this.wildState = 'normal';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
